43b146a83e7dad6285bca377d2c53267faa00965
Elom Agboka <opentech.consult@gmail.com>
2023-07-20 01:32:14 +0000

    custom middleware
    created a logger for our application that writes each request to the
    console.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import { router as movieRouter } from './movie/index.js';
 
 const app = express();
 
+function log(request, response, next) {
+    console.log(request.url);
+    next();
+}
+
+app.use(log);
+
 app.use('/movie', movieRouter);
 
 app.get('/', (request, response) => response.redirect('/movie'));
